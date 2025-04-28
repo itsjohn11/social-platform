c72bc6846d32556935bed6d468883fa2a729823f
Giovanni ALESI <giovannialesi87@icloud.com>
2025-04-28 15:21:18 +0200

    modifica esercizi

--- social_platform.sql
+++ social_platform.sql
@@ -3,6 +3,7 @@ SELECT*
 FROM `medias`
 WHERE `type` LIKE "video"
 
+
 -- 2 Seleziona tutti i post senza Like (13)
 SELECT *
 FROM `posts`
@@ -10,18 +11,15 @@ LEFT JOIN `likes`
 ON `posts`.`id`= `likes`.`post_id`
 WHERE `likes`. `post_id` IS NULL
 
+
 -- 3 Conta il numero di like per ogni post (165 nel caso di query con SELECT + JOIN oppure 175 nel caso di query con sola SELECT)
-SELECT *
-FROM `posts`
-INNER JOIN `likes`
-ON `posts`.`id`= `likes`.`post_id`
--- (da completare)
 
 SELECT `post_id`,COUNT(*)
 FROM `posts`
 INNER JOIN `likes`
 ON `posts`.`id`= `likes`.`post_id`
 GROUP BY `post_id`;
+
 
 -- 4 Ordina gli utenti per il numero di media caricati (25)
 SELECT *
@@ -38,12 +36,7 @@ ORDER BY `medias`.`user_id` ASC
 
 
 -- 5 Ordina gli utenti per totale di likes ricevuti nei loro posts (25)
-SELECT  `post_id`, `user_id`, COUNT(`likes`.`id`)
-FROM `users`
-INNER JOIN `likes`
-ON `users`.`id` = `likes`.`user_id`
-ORDER BY `user_id`,`post_id`;
------->o
+
 SELECT  `users`.`id`, COUNT(`likes`.`id`)
 FROM `users`
 INNER JOIN `likes`
